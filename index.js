abc95329be4316315edc1ed992c0917ae2fbf01e
P Paul Jonathan <ppauljonathan@gmail.com>
2020-12-13 20:35:21 +0530

    added an image and now can convert images to grayscale

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
+img=new Image;
+img.src="./ghjk.jpg";
+img.onload=function(){
 canv=document.createElement("canvas");
-canv.width=500;canv.height=500;
+canv.width=img.width;canv.height=img.height;
 document.body.appendChild(canv);
 c=canv.getContext("2d");
-c.fillStyle="#000";
-c.fillRect(0,0,canv.width,canv.height);
-c.fillStyle="#0f0";
-c.fillRect(0,0,200,50);
-c.fillStyle="#f00";
-c.fillRect(50,200,100,30);
-c.fillStyle="#00f";
-c.fillRect(150,400,10,30);
+c.drawImage(img,0,0);}
 function gray()
 {
     d=c.getImageData(0,0,canv.width,canv.height);
