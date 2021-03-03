7ff2e574532ea30fa6a222414355734e73fe01c6
P Paul Jonathan <70207656+ppauljonathan@users.noreply.github.com>
2021-03-03 11:04:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 img=new Image;
-img.src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg";
+img.src="image.jpg";
 img.onload=function(){
 canv=document.createElement("canvas");
 canv.width=img.width;canv.height=img.height;
